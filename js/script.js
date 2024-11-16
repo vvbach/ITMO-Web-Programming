65bc10fcff4b5f4e5de9a61397bb1c8ce40c4d5f
vvbach <101302215+vvbach@users.noreply.github.com>
2024-11-16 12:01:38 +0300

    Add dropdown button

--- js/script.js
+++ js/script.js
@@ -10,7 +10,21 @@ function setFooterAtBottom(){
         footer.classList.remove('bottom');
     }
 }
-
 document.addEventListener("DOMContentLoaded", setFooterAtBottom);
+window.onresize = setFooterAtBottom;
 
-window.onresize = setFooterAtBottom
+
+function clickMenuBtn(){
+    document.getElementsByClassName('dropdown-btn')[0].classList.toggle('dropdown-btn-change-animation');
+    document.getElementsByClassName('dropdown-content')[0].classList.toggle('dropdown-content-open');
+}
+
+function setPositionDropdownMenu(){
+    var dropdownContent = document.getElementsByClassName('dropdown-content')[0];
+    var topMenuContainer = document.getElementsByClassName('top-menu-container')[0];
+
+    dropdownContent.style.top = topMenuContainer.getBoundingClientRect().bottom + 'px'; 
+}
+
+document.addEventListener("DOMContentLoaded", setPositionDropdownMenu);
+window.onresize = setPositionDropdownMenu;
